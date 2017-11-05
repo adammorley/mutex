300f53c1ad41adcff26f2cf4d350410dd7b8b607
Adam Morley <adam@Adams-MacBook.local>
2017-11-04 22:28:06 -0700

    update structure packing, clean up vars

--- mutex-thread-test.c
+++ mutex-thread-test.c
@@ -6,8 +6,8 @@
 #include "mutex.h"
 
 struct t_args {
-    mutex* m;
     int* x;
+    mutex* m;
 };
 
 void* update(void* args) {
@@ -19,29 +19,27 @@ void* update(void* args) {
 }
 
 int main(void) {
-    mutex* m = mutex_create();
-    int* x = malloc(sizeof(int));
-    *x = 0;
-
     struct t_args args;
-    args.m = m;
-    args.x = x;
+    args.m = mutex_create();
+    args.x = malloc(sizeof(int));
+    *args.x = 0;
 
     pthread_t tid0;
     pthread_t tid1;
     pthread_create(&tid0, NULL, update, (void*) &args);
     usleep(50);
-    assert(mutex_islocked(m));
-    assert(*x == 1);
+    assert(mutex_islocked(args.m));
+    assert(*args.x == 1);
     pthread_create(&tid1, NULL, update, (void*) &args);
-    assert(*x == 1);
-    assert(mutex_islocked(m));
+    assert(*args.x == 1);
+    assert(mutex_islocked(args.m));
     usleep(50);
-    assert(*x == 2);
+    assert(*args.x == 2);
     usleep(150);
-    assert(!mutex_islocked(m));
+    assert(!mutex_islocked(args.m));
     pthread_join(tid0, NULL);
     pthread_join(tid1, NULL);
-    mutex_delete(m);
+    mutex_delete(args.m);
+    free(args.x);
     return 0;
 }
